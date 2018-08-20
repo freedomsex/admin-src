a786b06202c124c2214091aeb605ab1e62cc1395
tebaly <support@freedomsex.net>
2018-08-20 18:27:18 +0300

    stash-review-list-untested

---
+++ src/components/modules/review-list.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data: () => ({
+
+  }),
+  mounted() {
+    // do something after mounting vue instance
+
+  },
+  methods: {
+    methodName() {
+
+    },
+  },
+};
+</script>
+
+<template>
+  <div id="">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
